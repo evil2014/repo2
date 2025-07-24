c12f2dfb09340fcea36f9526bd486ca68dcbbd65
evil2014 <evaldivia.lugo@gmail.com>
2025-07-24 08:53:49 -0600

    chage port

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@
 echo  "Limpiando contenedores anteriores..."
 
 # Detener y eliminar los servicios definidos en docker-compose.prod.yml
-docker compose -f docker-compose.prod.yml down
+docker-compose -f docker-compose.prod.yml down
 
 echo " Desplegando servidor Nginx limpio..."
-docker compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml up -d --build
 
 echo "Despliegue completado correctamente."
 
